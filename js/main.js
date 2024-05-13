da463dbdcdb2748113ed9130066abc783d86bcad
TiagoSpada <tiago.spada.ts@gmail.com>
2024-05-13 17:19:41 +0200

    added script without bonus

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
 // Simulare un count down di 10 secondi che alla fine dice "Buon anno!"
 // Potete stampare il messaggio in pagina con un semplice alert.
 'use strict';
+const auguri = 'Buon anno!';
+
+setTimeout(function(){
+    alert(auguri);
+}, 10000);
